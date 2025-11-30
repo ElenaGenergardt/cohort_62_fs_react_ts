a60df644d9185c15a40977fc3a69d7656a7a479e
ElenaGenergardt <P_L_Gen@web.de>
2025-11-30 15:30:24 +0100

    Add homework 06

---
+++ src/homeworks/Homework_06/Homework_06.tsx
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+interface Car {
+  brand: string;
+  price: number;
+  isDiesel: boolean;
+}
+const cars: Car[] = [
+  { brand: "BMW", price: 20000, isDiesel: true },
+  { brand: "Mercedes", price: 22000, isDiesel: false },
+  { brand: "Porshe", price: 50000, isDiesel: true },
+  { brand: "Nissan", price: 25000, isDiesel: false },
+  { brand: "Audi", price: 50000, isDiesel: true },
+];
+
+const carsCard = cars.map((car, index) => {
+  return (
+    <div key={index} className="car_card">
+      <h2 className="car_brand">Модель : {car.brand}</h2>
+      <p className="car_price">Цена : {car.price}</p>
+      <p className="car_fuel">
+        Тип топлива : {car.isDiesel ? "Дизель" : "Бензин"}
+      </p>
+    </div>
+  );
+});
+
+function Homework_06() {
+  return (
+    <div className="cars_container">
+      <h1>Список автомобилей</h1>
+      {carsCard}
+    </div>
+  );
+}
+
+export default Homework_06;
