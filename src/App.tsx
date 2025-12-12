bdecf76b456d9e400a11d6bbd0e244649d54dc0d
ElenaGenergardt <P_L_Gen@web.de>
2025-12-12 19:57:53 +0100

    Add Lesson 10

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Lesson_07 from "lessons/Lesson_07/Lesson_07";
 import Lesson_08 from "lessons/Lesson_08/Lesson_08";
 import Homework_08 from "homeworks/Homework_08/Homework_08";
 import Lesson_09 from "lessons/Lesson_09/Lesson_09";
+import Lesson_10 from "lessons/Lesson_10/Lesson_10";
 
 // Homeworks
 // import Homework_06 from "./homeworks/Homework_06/Homework_06";
@@ -19,7 +20,8 @@ function App() {
   <GlobalStyles />
   {/* <Lesson_08 /> */}
   {/* <Homework_08 /> */}
-  <Lesson_09 />
+  {/* <Lesson_09 /> */}
+  <Lesson_10 />
   </>
   );
 }
